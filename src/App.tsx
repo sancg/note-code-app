5971b2a9a54476e2d12c364c75e6cce08959c245
Santiago Cano <sanblackx1@gmail.com>
2024-05-28 23:29:34 -0400

    ADD: Skeleton when loading Editor

--- src/App.tsx
+++ src/App.tsx
@@ -1,41 +1,22 @@
-import { useEffect, useRef, useState } from 'react';
-import { editor } from 'monaco-editor';
 import './App.css';
-import { Editor } from '@monaco-editor/react';
+import * as monacoEditor from 'monaco-editor';
+
+import { Editor, OnMount } from '@monaco-editor/react';
+import { useRef, useState } from 'react';
+import { AspectRatio, Box, Card, Skeleton } from '@mui/joy';
 
 function App() {
-  const editorRef = useRef(null);
+  const editorRef = useRef<monacoEditor.editor.IStandaloneCodeEditor | null>(null);
+  const [codeSnippet, setCodeSnippet] = useState('');
 
-  /*useEffect(() => {
-    if (editorRef.current) {
-      // Crear una instancia del editor
-      const editor = monaco.editor.create(editorRef.current, {
-        value: 'console.log("¡Hola, mundo!");',
-        language: 'javascript',
-        theme: 'vs-light', // Puedes cambiar el tema según tus preferencias
-        automaticLayout: true,
-        padding: {
-          top: 20,
-          bottom: 20,
-        },
-        scrollbar: {
-          vertical: 'auto',
-          verticalHasArrows: false,
-          verticalScrollbarSize: 5,
-        },
-      });
+  const onMount: OnMount = (editor) => {
+    editorRef.current = editor;
+    editor.focus();
+  };
 
-      // Opcional: puedes configurar más opciones del editor aquí
-
-      return () => {
-        // Limpiar el editor cuando el componente se desmonte
-        editor.dispose();
-      };
-    }
-  }, []);*/
-  function handleEditorChange(value: string | undefined) {
-    console.log('here is the current model value:', value);
-  }
+  const handleEditorChange = (value: string | undefined) => {
+    console.log(value);
+  };
 
   return (
     <main>
@@ -44,10 +25,12 @@ function App() {
       <h2>Your Code easily</h2>
       {/* <article ref={editorRef} className='editor'> */}
       <Editor
-        // height='100vh'
+        height='100'
+        theme='vs-dark'
         className='editor'
         defaultLanguage='javascript'
         defaultValue=''
+        onMount={onMount}
         onChange={handleEditorChange}
         options={{
           padding: {
@@ -59,6 +42,19 @@ function App() {
             verticalScrollbarSize: 5,
           },
         }}
+        loading={
+          <Card className='editor' sx={{ width: '100%', overflowY: 'hidden' }}>
+            <AspectRatio ratio='9/11'>
+              <Skeleton
+                variant='overlay'
+                animation={'wave'}
+                // sx={{ width: '80%', height: '90%' }}
+              >
+                <img alt='' src='data:image/gif;base64,R0lGODlhAQABAAD/ACwAAAAAAQABAAACADs=' />
+              </Skeleton>
+            </AspectRatio>
+          </Card>
+        }
       />
       {/* </article> */}
     </main>
